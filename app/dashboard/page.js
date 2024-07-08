2b783ac94938620092d874a1237a986786ddd4bb
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-07-08 11:57:23 -0300

    button and calendar adjst css, dashboard css and nav separation

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,30 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import Nav from '@/app/dashboard/nav/Nav';
+import Calendar from '@/components/Calendar';
+import { signOut } from 'next-auth/react';
+import c from './Dashboard.module.css'
 
 export default function page() {
+  const [view, setView] = useState('dashboard');
+
+  useEffect(() => {
+    if(view === 'signout'){
+      signOut();
+    }
+  }, [view])
+
+  function displayModal() {
+    setShowModal(true);
+  }
+
   return (
-    <Nav />
+    <div className={c.container}>
+      <Nav setView={setView} />
+      <div className={c.rowRight}>
+      {view === 'dashboard' && <Calendar />}
+    </div>
+    </div>
   )
 }
