f8093eff16537097e78826374ab8a5d0c1e365cb
Marcelo Araujo <codetrsh@gmail.com>
2024-07-29 00:52:04 -0300

    delete images from gallery from dashboard gallery, gallery modal mobile update, fetchImages set on utils

--- app/components/GalleryModal.js
+++ app/components/GalleryModal.js
@@ -1,65 +1,81 @@
 'use client';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import yachtImages from '@/imageImports';
-import c from './GalleryModal.module.css'
+import c from './GalleryModal.module.css';
 
-export default function ModalImage({isOpen, selectedImage, onClose, initialIndex}) {
-  const imgArray = Object.values(yachtImages); // imgArray[index] is the object for the src
+export default function ModalImage({
+  isOpen,
+  selectedImage,
+  onClose,
+  initialIndex,
+  imagesUrl
+}) {
+  
   const [currentIndex, setCurrentIndex] = useState(initialIndex);
-  const [srcObject, setSrcObject] = useState(imgArray[initialIndex]);
 
-  function handleArrowClick(direction){
-    
-    if(direction === 'right'){
-      
-      if(currentIndex >= imgArray.length -1){
-        setCurrentIndex(prevIndex => {
+  function handleArrowClick(direction) {
+    if (direction === 'right') {
+      // last element in the array
+      if (currentIndex >= imagesUrl.length - 1) {
+        setCurrentIndex((prevIndex) => {
           prevIndex = 0;
-          setSrcObject(imgArray[0])
-          return 0
-        })
-      }else{
-        setCurrentIndex(prevIndex => prevIndex + 1)
-        setSrcObject(imgArray[currentIndex + 1])
+          return 0;
+        });
+      } else {
+        setCurrentIndex((prevIndex) => prevIndex + 1);
       }
     }
 
-    if(direction === 'left'){
-      if(currentIndex <= 0){
-        setCurrentIndex(prevIndex => {
-          const newIndex = imgArray.length -1;
-          setSrcObject(imgArray[newIndex])
-          return newIndex
-        })
-      }else{
-        setCurrentIndex(prevIndex => prevIndex -1);
-        setSrcObject(imgArray[currentIndex -1])
+    if (direction === 'left') {
+      if (currentIndex <= 0) {
+        setCurrentIndex((prevIndex) => {
+          prevIndex = imagesUrl.length - 1;
+          const newIndex = imagesUrl.length - 1;
+          return newIndex;
+        });
+      } else {
+        setCurrentIndex((prevIndex) => prevIndex - 1);
       }
-    } 
+    }
   }
 
   return (
     <>
-    {isOpen && (
-      <div className={c.modalContainer}>
-      <div className={c.modalContent}>
-        <div className={c.x}>
-        <Image 
-        className={c.modalImg}
-        src={srcObject}
-        alt='enlarged img'
-        width={800}
-        style={{objectFit: "contain", maxHeight: '600px'}}
-        />
+      {isOpen && (
+        <div className={c.modalContainer}>
+          <div className={c.modalContent}>
+            <div className={c.x}>
+              <Image
+                className={c.modalImg}
+                src={imagesUrl[currentIndex]}
+                alt="enlarged img"
+                width={800}
+                height={500}
+                style={{ objectFit: 'contain', maxHeight: '600px' }}
+              />
+            <div className={c.modalControls}>
+              <button className={c.btnClose} onClick={onClose}>
+                X
+              </button>
+              <div
+                className={c.leftArrow}
+                onClick={() => handleArrowClick('left')}
+              >
+                <i className="bi bi-arrow-left"></i>
+              </div>
+              <div
+                className={c.righttArrow}
+                onClick={() => handleArrowClick('right')}
+              >
+                <i className="bi bi-arrow-right"></i>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className={c.modalBackground} onClick={onClose}></div>
         </div>
-        <button className={c.btnClose} onClick={onClose}>X</button>
-        <div className={c.leftArrow} onClick={() => handleArrowClick('left')}><i className="bi bi-arrow-left"></i></div>
-        <div className={c.righttArrow} onClick={() => handleArrowClick('right')}><i className="bi bi-arrow-right"></i></div>
-      </div>
-      <div className={c.modalBackground} onClick={onClose}></div>
-    </div>
-    )}
+      )}
     </>
-  )
+  );
 }
---
+++ app/dashboard/gallery/GallerySettings.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react';
+import Button from '@/app/components/Button';
+import Image from 'next/image';
+import img from '@/public/images/main.JPG';
+import fetchImages from '@/app/utils/fetchImages';
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  listAll,
+  deleteObject,
+} from 'firebase/storage';
+import c from './GallerySettings.module.css';
+
+
+const storage = getStorage();
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const storageRef = ref(storage, storageBucket);
+
+
+export default function GallerySettings() {
+  const [pictures, setPictures] = useState([]);
+
+
+  useEffect(() => {
+    const getImages = async () => {
+      const urls = await fetchImages();
+      setPictures(urls);
+    };
+    getImages();
+  }, []);
+
+  async function handleDelete(img) {
+    const parts = img.split('/');
+    const fileName = parts[parts.length - 1].split('?')[0];
+
+    // Create a reference to the file to delete
+    const desertRef = ref(storageRef, fileName);
+    console.log(desertRef)
+
+    // Delete the file
+    deleteObject(desertRef)
+      .then(() => {
+        // File deleted successfully
+        console.log('file deleted successfully');
+        setPictures((prevPictures) => prevPictures.filter((picture) => picture !== img));
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+        console.log('Something went wrong deleting the file')
+      });
+  }
+  return (
+    <div className={c.container}>
+      <div></div>
+      <Button
+        className={c.btn}
+        // onClick={() => handleDelete(picture._id)}
+      >
+        Add
+      </Button>
+      {pictures.map((picture) => {
+        return (
+          <div className={c.row} key={picture}>
+            <div className={c.pictureContainer}>
+              <Image
+                src={picture}
+                className={c.picture}
+                alt="img"
+                height={100}
+                width={150}
+              />
+            </div>
+            <div className={c.btnContainer}>
+              <Button
+                color="red"
+                className={c.btn}
+                onClick={() => handleDelete(picture)}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
