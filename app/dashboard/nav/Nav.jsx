a305aa43917e426b0aed147d564bd8e502bb7804
Marcelo Araujo <codetrsh@gmail.com>
2024-08-22 20:16:59 -0400

    conflict fixed

--- app/dashboard/nav/Nav.jsx
+++ app/dashboard/nav/Nav.jsx
@@ -52,10 +52,7 @@ export default function Nav({ setView, resetView, onNavClick }) {
         <div className={c.col} onClick={() => { setView('checklist')
           resetView();
           onNavClick();
-<<<<<<< HEAD
-=======
           setResetCheckList(!resetCheckList);
->>>>>>> wip
           }}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
