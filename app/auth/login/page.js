27774415ed0a713de460fc06cb8208d9389f4f4d
Marcelo Araujo <codetrsh@gmail.com>
2024-09-21 14:39:16 -0400

    auth checking at login

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Login from '../../components/Login';
 import PageContent from '@/app/components/PageContent';
+import { redirect } from 'next/navigation';
 import c from './page.module.css'
-export default function loginPage() {
+export default async function loginPage() {
+  const session = await getServerSession(authOptions);
+// If the user is already authenticated, redirect to the dashboard
+if (session) {
+  redirect('/dashboard')
+}
   return (
-
     <PageContent>
       <Login />
     </PageContent>
